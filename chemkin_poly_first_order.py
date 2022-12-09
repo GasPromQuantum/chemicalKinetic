2913f758954bef53529105ff0309183820326472
Kind Shark <kinder@LAPTOP-V952G07H.localdomain>
2022-12-09 23:00:17 +0300

    Refactor first order polynomial method

--- chemkin_poly_first_order.py
+++ chemkin_poly_first_order.py
@@ -1,99 +1,58 @@
 import json
-import numpy as np
 import scipy.special
 import qubovert as qv
-
-with open('Data/ChemKin/first_order.json') as f:
-    data = json.load(f)
-
-time = np.array(data['time'])
-con = np.array(data['concentrations'])
-N = len(time)
-M = len(con[0])
-K = 1  # number of k's
-n = 10  # digits in k  + 1
-
-N_add = 4# number of additional dots between each pair
-          # todo: choose with |1-kdt*dT|<1 && dt< min(dT_i) !!!  
-# dT = 1
-# r = int(dT/dt)
-k_min = 0.125
-k_max = 2
-
-k_vars = [qv.PUBO.create_var('k%d' % i) for i in range(n)] #k = [kmin,kmax-step]
-k = -sum((0.5 ** i) * k_vars[i] for i in range(n))
-print(k_vars)
-print("type(k) = ", type(k))
-#for j in range(3):
-#    print(k**j)      #doesn't work :(
-
-temp = qv.PUBO()
-temp[()] = 1
-k_pows = []
-for i in range(N_add+2):
-    #print("i=",i,"k^i=",temp)
-    print("============temp============")
-    print(temp)
-    print("============================")
-    k_pows.append(temp)
-    temp = temp*k
-
-print("---------------k_pows-------------")
-print("k_pows[0]: ",k_pows[0])
-print("k_pows[1]: ",k_pows[1])
-print("k_pows[2]: ",k_pows[2])
-print("----------------------------------")
-
-
-F = 0
-
-for i in range(N-1): #!!!!!!!!
-    delta = con[i+1][0]
-    dt = (time[i+1]-time[i]) / (N_add + 1)
-    #print("dt =",dt)
-    for j in range(N_add+2): # +1 !!!
-        C = scipy.special.binom(N_add+1, j)
-        
-        #delta -= C * con[i - 1][0] * (dt * k)**j
-        delta -= C * con[i][0] * (dt**j) * k_pows[j]
-    #print(i, delta)
-    F += delta ** 2
-
-print("F =")
-print(F)
-
-print()
-int_F = F.to_pubo()
-print("F vars:", F.variables)
-print("int_F vars:", int_F.variables)
-
-"""
-int_F_solution = int_F.solve_bruteforce()
-print("bruteforce solution:", int_F_solution)
-F_solution = F.convert_solution(int_F_solution)
-print(F_solution)
-print()
-print("k =", k.value(F_solution))
-print("F(k) =", F.value(F_solution))
-"""
-print("degree =",F.degree) 
-
-print()
-print("anneal_pubo solution:")
-res = qv.sim.anneal_pubo(F, num_anneals=1000)
-print(res.best)
-print()
-
-F_solution = res.best.state
-print("F_solution =", F_solution)
-print("k =", k.value(F_solution))
-print("F(k) =", F.value(F_solution))
-
-
-
+from typing import List
 import matplotlib.pyplot as plt
-plt.hist([sample.value for sample in res])
-plt.xlabel("$F$")
-plt.ylabel("counts")
-plt.title("Values of $F$ sampled")
-plt.show()
+
+
+def k_pows_list(k: qv._pubo.PUBO, n_pows: int) -> List[qv._pubo.PUBO]:
+    temp = qv.PUBO()
+    temp[()] = 1
+    return [temp] + [k**j for j in range(1, n_pows)]
+
+
+def get_mininization_func(cons: list, times: list, k_pows: list, N_exp: int, N_add: int) -> qv._pubo.PUBO:
+    F = 0
+
+    for i in range(N_exp - 1):  # !!!!!!!!
+        delta = cons[i + 1][0]
+        dt = (times[i + 1] - times[i]) / (N_add + 1)
+        for j in range(N_add + 2):  # +1 !!!
+            C = scipy.special.binom(N_add + 1, j)
+            delta -= C * cons[i][0] * (dt ** j) * k_pows[j]
+
+        F += delta ** 2
+
+    return F
+
+
+def main():
+    with open('first_order.json') as f:
+        data = json.load(f)
+
+        time = data['time']
+        con = data['concentrations']
+
+        N = len(time)  # number of experimental points
+        n = 10  # number of binary digits, representing k
+        # number of additional dots between each pair of experimental dots
+        # todo: choose with |1-kdt*dT|<1 && dt< min(dT_i)
+        N_add = 4
+
+        k_bin = [qv.PUBO.create_var('k%d' % i) for i in range(n)]
+
+        k_dec = -sum((0.5 ** i) * k_bin[i] for i in range(n))
+        k_pows = k_pows_list(k_dec, N_add+2)
+
+        F = get_mininization_func(con, time, k_pows, N, N_add)
+        res = qv.sim.anneal_pubo(F, num_anneals=1000)
+
+        plt.hist([sample.value for sample in res])
+        plt.xlabel("$F$")
+        plt.ylabel("counts")
+        plt.title("Values of $F$ sampled")
+        plt.show()
+
+
+if __name__ == "__main__":
+    main()
